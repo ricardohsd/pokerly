c72c4a58c943a9cefa493ee8595aa5da9c6e87b8
Ricardo Domingos <ricardohsd@gmail.com>
2020-04-09 19:09:31 +0000

    Initial commit

---
+++ lib/game.ex
@@ -0,0 +1,18 @@
+defmodule Pokerly do
+  @moduledoc """
+  Documentation for `Pokerly`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Pokerly.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/game_test.exs
@@ -0,0 +1,8 @@
+defmodule PokerlyTest do
+  use ExUnit.Case
+  doctest Pokerly
+
+  test "greets the world" do
+    assert Pokerly.hello() == :world
+  end
+end
